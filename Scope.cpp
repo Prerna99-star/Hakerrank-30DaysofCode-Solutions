01027045799f1c812b12e85fb4e8dff9ee320cec
Prerna Mishra <mishraprerna218@gmail.com>
2020-07-15 12:57:13 +0530

    Day 14

---
+++ Scope.cpp
@@ -0,0 +1,55 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+
+	Difference(vector <int> elements){
+     this->elements = elements;
+    }
+    public:
+    void computeDifference(){
+        int diff = 0, maximumdiff = 0;
+        for(int i=0; i< elements.size(); i++){
+            for(int j=i+1; j< elements.size(); j++){
+                diff = abs(elements[i]-elements[j]);
+                if(maximumdiff < diff){
+                    maximumdiff = diff;
+                }
+            }
+        }
+        maximumDifference = maximumdiff;
+    }
+
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
